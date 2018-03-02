80fb4e997fc5dcf25bc7c60403fc409681bee0e6
sarath <sarath@cabin-holiday.com>
2018-03-02 07:10:35 +0100

    API-44: Css updates

--- resources/views/cabinDetails.blade.php
+++ resources/views/cabinDetails.blade.php
@@ -267,7 +267,7 @@
                                     @endif
                                 </div>
 
-                                <div class="thumbnail">
+                                <div class="thumbnail" style="border:none; border-radius:0px; background-color: #EFEFEF;">
                                     <ul id="image-gallery" class="gallery list-unstyled cS-hidden">
                                         <li data-thumb="https://placehold.it/250x180">
                                             <img src="https://placehold.it/600x400" style="width:100%;" class="img-responsive">
