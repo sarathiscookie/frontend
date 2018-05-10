57010a299def376b0af36e441736f3a134a78e38
sarath <sarath@cabin-holiday.com>
2018-05-10 10:48:05 +0200

    API-50: Testing payment response in dev server

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-/* Response from payone */
-Route::post('/payment/response', 'PaymentController@response')->name('payment.response');
