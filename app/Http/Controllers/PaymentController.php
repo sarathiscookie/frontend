51f0a1447164cdd25a23d6b9a17d690964004020
sarath <sarath@cabin-holiday.com>
2018-05-14 09:46:06 +0200

    API-50: Testing payment response in dev server

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -337,7 +337,7 @@ class PaymentController extends Controller
 
             "portalid"    => env('PORTAL_ID'),
 
-            "key"         => env('KEY'), // The key has to be hashed as md5
+            "key"         => hash("md5", env('KEY')), // The key has to be hashed as md5
 
             "mode"        => env('MODE'), // Can be "live" for actual transactions
 
