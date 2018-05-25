9e2a5f2153492fbb9456075e0d3514ecdc321bd4
sarath <sarath@cabin-holiday.com>
2018-05-25 12:06:47 +0200

    API-46: Html required removed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
                             <label for="email" class="col-md-4 control-label">{{ __('login.email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ __('login.emailPlaceholder') }}">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"  autofocus placeholder="{{ __('login.emailPlaceholder') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -41,7 +41,7 @@
                             <label for="password" class="col-md-4 control-label">{{ __('login.password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" placeholder="{{ __('login.passwordPlaceholder') }}" required>
+                                <input id="password" type="password" class="form-control" name="password" placeholder="{{ __('login.passwordPlaceholder') }}" >
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,7 @@
                             <label for="firstName" class="col-md-4 control-label">{{ __('register.firstname') }}</label>
 
                             <div class="col-md-6">
-                                <input id="firstName" type="text" class="form-control" name="firstName" value="{{ old('firstName') }}" placeholder="{{ __('register.firstnamePlaceholder') }}" required autofocus>
+                                <input id="firstName" type="text" class="form-control" name="firstName" value="{{ old('firstName') }}" placeholder="{{ __('register.firstnamePlaceholder') }}"  autofocus>
 
                                 @if ($errors->has('firstName'))
                                     <span class="help-block">
@@ -31,7 +31,7 @@
                             <label for="lastName" class="col-md-4 control-label">{{ __('register.lastname') }}</label>
 
                             <div class="col-md-6">
-                                <input id="lastName" type="text" class="form-control" name="lastName" value="{{ old('lastName') }}" placeholder="{{ __('register.lastnamePlaceholder') }}" required autofocus>
+                                <input id="lastName" type="text" class="form-control" name="lastName" value="{{ old('lastName') }}" placeholder="{{ __('register.lastnamePlaceholder') }}"  autofocus>
 
                                 @if ($errors->has('lastName'))
                                     <span class="help-block">
@@ -45,7 +45,7 @@
                             <label for="email" class="col-md-4 control-label">{{ __('register.email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ __('register.emailPlaceholder') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ __('register.emailPlaceholder') }}" >
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -59,7 +59,7 @@
                             <label for="password" class="col-md-4 control-label">{{ __('register.password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" placeholder="******" required>
+                                <input id="password" type="password" class="form-control" name="password" placeholder="******" >
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
@@ -73,7 +73,7 @@
                             <label for="password-confirm" class="col-md-4 control-label">{{ __('register.cpassword') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="******" required>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="******" >
                             </div>
                         </div>
 
