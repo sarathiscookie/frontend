8283b3585cbdd0d490fe724accd50225574b9d4c
sarath <sarath@cabin-holiday.com>
2018-07-27 13:53:29 +0200

    API-55: Booking list Cron

--- app/Console/Commands/BookingListForCabins.php
+++ app/Console/Commands/BookingListForCabins.php
@@ -83,7 +83,7 @@ class BookingListForCabins extends Command
             /*to('michael@hofer-werbung.de')->bcc('iamsarath1986@gmail.com')->cc('l.linder@huetten-holiday.de')*/
             /*to('iamsarath1986@gmail.com')*/
             Mail::send('emails.bookingListCabin', ['subject' => 'Ihre tägliche Buchungsübersicht'], function ($message) use ($cabinOwner, $cabin) {
-                $message->bcc('backup.tageslisten@huetten-holiday.de')->to($cabinOwner->usrEmail)->subject('Ihre tägliche Buchungsübersicht')->attach(public_path("/storage/dailylistbookingforcabin/". $cabin->name . ".pdf"), [
+                $message->to('iamsarath1986@gmail.com')->subject('Ihre tägliche Buchungsübersicht')->attach(public_path("/storage/dailylistbookingforcabin/". $cabin->name . ".pdf"), [
                     'mime' => 'application/pdf',
                 ]);
             });
