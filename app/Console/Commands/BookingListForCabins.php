0e86362a65fb520621b9166df003e41faa68e567
sarath <sarath@cabin-holiday.com>
2018-10-17 13:15:42 +0200

    API-55: Added condition: If bookings available then email attached pdf send. Testing

--- app/Console/Commands/BookingListForCabins.php
+++ app/Console/Commands/BookingListForCabins.php
@@ -89,13 +89,13 @@ class BookingListForCabins extends Command
                     ->where('usrlId', 5)
                     ->find($cabin->cabin_owner);
 
-                if(!empty($cabinOwner)) {
+                if((!empty($cabinOwner)) && ( count($bookings) > 0 || count($msBookings) > 0)) {
                     /* Functionality to send message to user begin */
                     /*to($cabinOwner->usrEmail)->bcc('backup.tageslisten@huetten-holiday.de')*/
                     /*to('l.linder@huetten-holiday.de')->bcc('iamsarath1986@gmail.com')*/
                     /*to('iamsarath1986@gmail.com')*/
                     Mail::send('emails.bookingListCabin', ['subject' => 'Ihre tägliche Buchungsübersicht'], function ($message) use ($cabinOwner, $cabin) {
-                        $message->to($cabinOwner->usrEmail)->bcc('backup.tageslisten@huetten-holiday.de')->subject('Ihre tägliche Buchungsübersicht')->attach(public_path("/storage/dailylistbookingforcabin/". $cabin->name . ".pdf"), [
+                        $message->to('iamsarath1986@gmail.com')/*to($cabinOwner->usrEmail)->bcc('backup.tageslisten@huetten-holiday.de')*/->subject('Ihre tägliche Buchungsübersicht')->attach(public_path("/storage/dailylistbookingforcabin/". $cabin->name . ".pdf"), [
                             'mime' => 'application/pdf',
                         ]);
                     });
