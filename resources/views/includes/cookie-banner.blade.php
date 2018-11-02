d18f420d20e65902781be87d99a3100fe69be77a
Vlad Temian <v.temian@proxima-idea.com>
2018-11-02 09:59:18 +0200

    added translations + styling

--- resources/views/includes/cookie-banner.blade.php
+++ resources/views/includes/cookie-banner.blade.php
@@ -1,6 +1,7 @@
 <div class="cookie-banner">
-    <div class="cookie-message">
-        This website uses cookies to deliver our services. By using our site, you acknowledge that you have read and understand our <a href="{{ route('data.protection') }}"><strong>Data Protection Policy</strong></a>.  Disabling cookies may cause problems with some of the website's features.
-    </div>
-    <button type="button" class="btn btn-default" onclick="acceptCookies()">Accept cookies</button>
+    <p class="cookie-message">
+        {!! __('cookies.content') !!}
+    </p>
+    <a href="{{ route('data.protection') }}" class="btn btn-outline-default">{{ __('cookies.buttons.more') }}</a>
+    <button type="button" class="btn btn-default" onclick="acceptCookies()">{{ __('cookies.buttons.accept') }}</button>
 </div>
